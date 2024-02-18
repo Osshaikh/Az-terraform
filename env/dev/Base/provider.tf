a047839b5a09bcef300cc3c174070c3eda0ca568
Osama Sheikh <44756471+Osshaikh@users.noreply.github.com>
2024-02-18 13:35:40 +0530

    Update provider.tf
    update azurembackend terraofmr

--- env/dev/Base/provider.tf
+++ env/dev/Base/provider.tf
@@ -5,7 +5,14 @@ terraform {
       version = "=3.79.0"
     }
   }
-  backend "local" {}
+  terraform {
+  backend "azurerm" {
+    resource_group_name  = "AKS-LAB"
+    storage_account_name = "esseldatalakedemo"
+    container_name       = "tfstate"
+    key                  = "Tce9wOyo7/4ExC9ulJBmreEpKerzMYREYUFypt6eu1tmtM2l8zn3YkJmoNe7gymt/BtcXVEAEE2C+AStJL8nMw=="
+  }
+}
 }
 
   provider "azurerm" {
